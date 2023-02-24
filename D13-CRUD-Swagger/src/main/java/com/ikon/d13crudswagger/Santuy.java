e3079564a634c4219e1035e8683b570014eb920a
irfansettz <irfansettz@gmail.com>
2023-02-24 09:15:35 +0700

    +add docker file to d13

--- D13-CRUD-Swagger/src/main/java/com/ikon/d13crudswagger/Santuy.java
+++ D13-CRUD-Swagger/src/main/java/com/ikon/d13crudswagger/Santuy.java
@@ -1,7 +1,8 @@
 package com.ikon.d13crudswagger;
 
-import java.util.ArrayList;
-import java.util.List;
+import io.swagger.models.auth.In;
+
+import java.util.*;
 
 public class Santuy {
     public static List<Integer> fibonacci(Integer n){
@@ -21,8 +22,40 @@ public class Santuy {
         }
         return result;
     }
+    public static String reverseString(String s){
+        String result = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result += s.charAt(i);
+        }
+        return result;
+    }
+    public static Map<String, Map<String, Integer>> countString(String str){
+        Map<String, Map<String, Integer>> result = new HashMap<>();
+        Set<String> setString = new HashSet<>();
+        List<String> listString = new ArrayList<>();
+        for (int i = 0; i < str.length()-1; i++) {
+            setString.add(String.valueOf(str.charAt(i)));
+            listString.add(String.valueOf(str.charAt(i)));
+        }
+
+        for (String s: setString) {
+            int count = Collections.frequency(listString, s);
+            Map<String, Integer> value = new HashMap<>();
+            value.put("count", count);
+            if (count == 1) {
+                value.put("isUnique", 1);
+            } else {
+                value.put("isUnique", 0);
+            }
+            result.put(s, value);
+        }
+        return result;
+    }
     public static void main(String[] args) {
 //        System.out.println(fibonacci(10));
-        System.out.println(factorial(3));
+//        System.out.println(factorial(3));
+//        System.out.println(reverseString("contoh"));
+        System.out.println(countString("abbbccc"));
     }
+
 }
